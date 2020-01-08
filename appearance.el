62170447acff0e5aa7fd2acceeeae69717da741d
Olle Mårtensson <olle.martensson@gmail.com>
2020-01-09 00:49:58 +0100

    nicer looks

--- appearance.el
+++ appearance.el
@@ -5,8 +5,7 @@
 
 ;; Highlight current line
 (global-hl-line-mode 0)
-(set-face-attribute 'default nil :font "Monaco-14")
-
+(set-default-font "Source Code Pro 14")
 
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
@@ -26,6 +25,6 @@
 ;; Every time a window is started, make sure it get maximized
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(load-theme 'wheatgrass t)
+(load-theme 'dracula t)
 
 (provide 'appearance)
